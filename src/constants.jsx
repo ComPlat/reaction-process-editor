0914e381664ca236a9d54c056bc9616e68b03b59
Christian Buggle <christian@buggle.net>
2025-09-04 19:31:36 +0200

    Alter config path
    * Experimebntal for dokploy

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,4 @@
-import { chemotionElnHostname } from "./config";
+import { chemotionElnHostname } from "./files/config";
 // Set chemotionElnHostname to the running instance of your ELN server.
 export const apiHostname = chemotionElnHostname;
 export const apiBasePath = `${apiHostname}/api/v1/reaction_process_editor`;
