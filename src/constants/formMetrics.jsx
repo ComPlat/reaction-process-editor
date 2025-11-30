7a16e5912834cf05d5bd07b70bd892dadaa0b1bc
Christian Buggle <christian@buggle.net>
2025-11-30 18:15:15 +0100

    quotes

--- src/constants/formMetrics.jsx
+++ src/constants/formMetrics.jsx
@@ -1,12 +1,9 @@
+export const predefinableMetricNames = ['TEMPERATURE', 'PH', 'PRESSURE']
 
-// These conditions will be included in the conditionForm, in order of desired appearance.
-export const amountInputSetMetricNames = ['VOLUME', 'WEIGHT', 'MOLAR']
+export const addFormMetricNames = ['VELOCITY', 'TEMPERATURE', 'PRESSURE'];
+export const centrifugationFormMetricNames = ['MOTION', 'TEMPERATURE', 'PRESSURE'];
+export const removeFormMetricNames = ['TEMPERATURE', 'PRESSURE']
 
 export const conditionFormMetricNames = ['TEMPERATURE', 'PH', 'PRESSURE', 'IRRADIATION', 'MOTION', 'EQUIPMENT']
 
-export const addFormMetricNames = ["VELOCITY", "TEMPERATURE", "PRESSURE"];
-export const centrifugationFormMetricNames = ["MOTION", "TEMPERATURE", "PRESSURE"];
-
-export const removeFormMetricNames = ['TEMPERATURE', 'PRESSURE']
-
-export const predefinableMetricNames = ['TEMPERATURE', 'PH', 'PRESSURE']
+export const amountInputSetMetricNames = ['VOLUME', 'WEIGHT', 'MOLAR']
