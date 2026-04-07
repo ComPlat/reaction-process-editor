08d7cc60e7e3fcec5c77fa6350334cbefeaa8c03
Christian Buggle <christian@buggle.net>
2026-04-07 13:51:30 +0200

    Add Pagination

--- src/components/views/OntologiesEditor.jsx
+++ src/components/views/OntologiesEditor.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Input, Nav, NavItem, NavbarBrand, Button } from "reactstrap";
+import { Input, Nav, NavItem, NavbarBrand, Button, Label } from "reactstrap";
 
 import OntologyListItem from '../ontologies/OntologyListItem';
 import OntologyFormModal from '../ontologies/OntologyFormModal';
@@ -8,6 +8,8 @@ import OntologyFormModal from '../ontologies/OntologyFormModal';
 import { useReactionsFetcher } from "../../fetchers/ReactionsFetcher";
 
 import { SelectOptions } from '../../contexts/SelectOptions';
+
+import Select from 'react-select';
 
 const OntologiesEditor = () => {
   const [ontologies, setOntologies] = useState([])
@@ -26,6 +28,18 @@ const OntologiesEditor = () => {
   }
 
   const filteredOntologies = ontologies.filter((ont) => ontologyMatchesQuery(ont))
+
+  const [itemsPerPage, setItemsPerPage] = useState(100)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const perPageOptions = [{ value: 5, label: 5 }, { value: 10, label: 10 }, { value: 20, label: 20 }, { value: 50, label: 50 }, { value: 100, label: 100 }, { value: 200, label: 200 }, { value: 500, label: 500 }, { value: 10000, label: "All" }]
+
+  const totalPages = Math.max(1, Math.ceil(filteredOntologies.length / itemsPerPage))
+  const visiblePage = Math.max(1, Math.min(currentPage, totalPages))
+
+  const firstIndex = (visiblePage - 1) * itemsPerPage
+  const lastIndex = Math.min(firstIndex + itemsPerPage - 1, filteredOntologies.length)
+  const paginatedOntologies = filteredOntologies.slice(firstIndex, lastIndex)
 
   useEffect(() => {
     if (localStorage.getItem("bearer_auth_token")) {
@@ -52,37 +66,85 @@ const OntologiesEditor = () => {
 
   return (
     <>
-
       <SelectOptions.Provider value={{ ontologies: ontologies }}>
         <Nav fill className="navbar fixed-bottom bg-preparation px-5">
           <NavbarBrand>
-            {'Ontologies: ' + filteredOntologies.length + ' / ' + ontologies.length}
+            <Button onClick={e => setShowNewForm(true)}>+ Ontology</Button>
           </NavbarBrand>
-          <NavItem className="p-5">
+          <NavItem >
             <Input
+              className="mt-2 flex-grow-1"
               placeholder={'Search Ontologies'}
               value={ontologyQuery}
               onChange={(event) => setOntologyQuery(event.target.value)}
             />
           </NavItem>
           <NavItem>
-            <Button onClick={e => setShowNewForm(true)}>+ Ontology</Button>
+            <span >
+              {'Filtered: ' + filteredOntologies.length}
+              <br />
+              {'Total: ' + ontologies.length}
+            </span>
           </NavItem>
-        </Nav>
-        <Nav fill className="fixed-bottom bg-preparation px-5">
+          <NavItem >
+            <button
+              className="mx-2"
+              onClick={() => setCurrentPage(1)}
+              disabled={visiblePage === 1}
+            >
+              {'<<'}
+            </button>
+            <button
+              className="mx-2"
+              onClick={() => setCurrentPage(prev => Math.max(Math.min(totalPages, prev) - 1, 1))}
+              disabled={visiblePage === 1}
+            >
+              {'<'}
+            </button>
+
+
+          </NavItem>
           <NavItem>
-            <OntologyFormModal
-              key={"ontology_new"}
-              ontology={initialOntology}
-              isOpen={showNewForm}
-              onClose={e => setShowNewForm(false)}
-            />
+            <div>{(firstIndex + 1) + ' - ' + (lastIndex + 1)}</div>
+            <div>Page {visiblePage} of {totalPages}</div>
           </NavItem>
+          <NavItem>
+            <button
+              className="mx-2"
+              onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))}
+              disabled={visiblePage === totalPages}
+            >
+              {'>'}
+            </button>
+            <button
+              className="mx-2"
+              onClick={() => setCurrentPage(totalPages)}
+              disabled={visiblePage === totalPages}
+            >
+              {'>>'}
+            </button>
+          </NavItem>
+          <Select
+            className="react-select--overwrite"
+            classNamePrefix="react-select"
+            value={{ value: itemsPerPage, label: itemsPerPage }}
+            options={perPageOptions}
+            onChange={selected => setItemsPerPage(selected.value)}
+            menuPlacement={"auto"}
+          />
+          <Label className="p-2">Per Page</Label>
         </Nav>
+
+        <OntologyFormModal
+          key={"ontology_new"}
+          ontology={initialOntology}
+          isOpen={showNewForm}
+          onClose={e => setShowNewForm(false)}
+        />
 
         <table className="table table-striped w-auto">
           <tbody>
-            {filteredOntologies.map(ontology => {
+            {paginatedOntologies.map(ontology => {
               return (
                 <tr key={"ontology-list-row" + ontology.ontology_id}>
                   <OntologyListItem ontology={ontology} />
